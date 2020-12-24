c0b08ab2e1208155f45280c488d6bc7c576d2761
Kevin Paul Herbert <kph@platinasystems.com>
2020-12-23 16:36:47 -0800

    bmcd: Initial commit

---
+++ cmd/bmcd/bmcd.go
@@ -0,0 +1,68 @@
+// Copyright © 2020 Platina Systems, Inc. All rights reserved.
+// Use of this source code is governed by the GPL-2 license described in the
+// LICENSE file.
+
+package bmcd
+
+import (
+	"fmt"
+	"net/rpc"
+	"os"
+	"time"
+
+	"github.com/platinasystems/goes/lang"
+)
+
+type Command struct {
+}
+
+func (Command) String() string { return "bmcd" }
+
+func (Command) Usage() string {
+	return "daemons start bmcd [OPTION]..."
+}
+
+func (Command) Apropos() lang.Alt {
+	return lang.Alt{
+		lang.EnUS: "BMC Daemon",
+	}
+}
+
+func (Command) Man() lang.Alt {
+	return lang.Alt{
+		lang.EnUS: `
+DESCRIPTION
+	Management server on BMC`,
+	}
+}
+
+func (Command) Main(args ...string) (err error) {
+	var dev *os.File
+
+	for {
+		dev, err = os.OpenFile("/dev/i2c-slave-stream-0",
+			os.O_RDWR, 0)
+		if err == nil {
+			break
+		}
+		fmt.Printf("Error opening I2C device: %s\n", err)
+		time.Sleep(time.Second)
+	}
+	defer dev.Close()
+
+	client := rpc.NewClient(dev)
+
+	reply := ""
+	for {
+		err = client.Call("Swapper.SwapString",
+			"The quick brown fox did what?",
+			&reply)
+		if err != nil {
+			fmt.Printf("Error %s\n", err)
+		} else {
+			fmt.Println(reply)
+		}
+	}
+
+	return
+}
