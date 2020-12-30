e9e25105dffa6707397bf2c02c440e5c9fe521b3
Kevin Paul Herbert <kph@platinasystems.com>
2020-12-30 11:28:13 -0800

    checkpoint
    still broken

--- external/serial/sequencer_test.go
+++ external/serial/sequencer_test.go
@@ -5,8 +5,11 @@
 package serial
 
 import (
+	"bufio"
 	"fmt"
+	"math/rand"
 	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -21,8 +24,20 @@ func (t *SequencerTester) Read(p []byte) (n int, err error) {
 }
 
 func (t *SequencerTester) Write(p []byte) (n int, err error) {
-	t.msg <- p
-	t.msg <- p
+	r := rand.Intn(2)
+
+	switch r {
+	case 0:
+		fmt.Printf("SequenceTester.Write: Random is send message\n")
+		t.msg <- p
+	case 1:
+		fmt.Printf("SequenceTester.Write: Random is drop message\n")
+		break
+	case 2:
+		fmt.Printf("SequenceTester.Write: Random is duplicate message\n")
+		t.msg <- p
+		t.msg <- p
+	}
 
 	return len(p), nil
 }
@@ -33,10 +48,10 @@ func TestSequencerTest(t *testing.T) {
 
 	for i := 0; i < 10; i++ {
 		fmt.Printf("Pass %d\n", i)
-		p1 := []byte("This is a test")
-		p2 := []byte("If this were real, you'd know!")
+		p1 := fmt.Sprintf("Pass %d this is a test\n", i)
+		p2 := fmt.Sprintf("Pass %d If this were real, you'd know!\n", i)
 
-		n, err := sequencer.Write(p1)
+		n, err := sequencer.Write([]byte(p1))
 		if err != nil {
 			t.Error(err)
 			return
@@ -46,7 +61,7 @@ func TestSequencerTest(t *testing.T) {
 			return
 		}
 
-		n, err = sequencer.Write(p2)
+		n, err = sequencer.Write([]byte(p2))
 		if err != nil {
 			t.Error(err)
 			return
@@ -56,36 +71,39 @@ func TestSequencerTest(t *testing.T) {
 			return
 		}
 
-		buf := make([]byte, 128)
-		n, err = sequencer.Read(buf)
-		if err != nil {
-			t.Error(err)
-			return
+		scanner := bufio.NewScanner(sequencer)
+
+		if !scanner.Scan() {
+			t.Error("Scanner returned premature end")
+		}
+		t1 := scanner.Text()
+		if !reflect.DeepEqual(t1, strings.TrimSuffix(p1, "\n")) {
+			fmt.Printf("Pass %d data pattern 1 incorrect: got %v expected %v\n",
+				i, t1, p1)
+			t.Error("Data 1 incorrect\n")
+		} else {
+			fmt.Printf("Pass %d pattern 1 passes\n", i)
 		}
 
-		if n == 0 {
-			t.Error("Read returned zero")
-			return
+		if !scanner.Scan() {
+			t.Error("Scanner returned premature end")
 		}
+		t2 := scanner.Text()
+		if !reflect.DeepEqual(t2, strings.TrimSuffix(string(p2),
+			"\n")) {
+			fmt.Printf("Pass %d data pattern 2 incorrect: got %v expected %v\n",
+				i, t2, string(p2))
+			t.Error("Data 2 incorrect\n")
+		} else {
+			fmt.Printf("Pass %d pattern 2 passes\n", i)
+		}
+	}
 
-		if !reflect.DeepEqual(buf[:n], p1) {
-			t.Error("Data incorrect:", buf[:n], p1)
-		}
-
-		n, err = sequencer.Read(buf)
-		if err != nil {
-			t.Error(err)
-			return
-		}
-
-		if n == 0 {
-			t.Error("Read returned zero")
-			return
-		}
-
-		if !reflect.DeepEqual(buf[:n], p2) {
-			t.Error("Data incorrect:", buf[:n], p2)
-		}
+	scanner := bufio.NewScanner(sequencer)
+	if scanner.Scan() {
+		fmt.Printf("Final Expected EOF, received %v\n",
+			scanner.Text())
+		t.Error("Scanner should have returned EOF but didn't")
 	}
 	return
 }
