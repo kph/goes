0a7ed809a61ae3d2f382fa427bf64f75997e617d
Kevin Paul Herbert <kph@platinasystems.com>
2021-01-03 12:45:22 -0800

    sequencer: quiet the debugging

--- external/serial/sequencer.go
+++ external/serial/sequencer.go
@@ -69,7 +69,7 @@ func (s *Sequencer) runTimer() {
 		s.recvCond.L.Unlock()
 
 		s.xmitCond.L.Lock()
-		fmt.Printf("Sequencer.runTimer: len(s.rxmtBuf)=%d seqXmt=%d seqRxmt=%d seqRcv=%d\n",
+		s.debugf("Sequencer.runTimer: len(s.rxmtBuf)=%d seqXmt=%d seqRxmt=%d seqRcv=%d\n",
 			len(s.rxmtBuf), s.seqXmt, s.seqRxmt, ack)
 
 		seq := s.seqRxmt
@@ -108,7 +108,7 @@ func (s *Sequencer) runTimer() {
 			o = o[nn:]
 		}
 
-		fmt.Printf("Sent retransmit of seq %d ack %d s.seqXmt %d s.seqRxmt %d s.seqRcv %d len(s.rxmtBuf) %d\n",
+		s.debugf("Sent retransmit of seq %d ack %d s.seqXmt %d s.seqRxmt %d s.seqRcv %d len(s.rxmtBuf) %d\n",
 			seq, ack, s.seqXmt, s.seqRxmt, s.seqRcv, len(s.rxmtBuf))
 	}
 }
@@ -123,13 +123,13 @@ func (s *Sequencer) backgroundRead() {
 		default:
 		}
 		nn, err := s.c.Read(readbuf)
-		fmt.Printf("s.c.Read(readbuf()) returned len %d err %s\n", nn,
+		s.debugf("s.c.Read(readbuf()) returned len %d err %s\n", nn,
 			err)
 		if err != nil || nn < 8 {
 			if err == nil {
 				err = fmt.Errorf("Bad read length %d", nn)
 			}
-			fmt.Printf("Exiting backgroundRead: error reading readbuf: %s\n",
+			s.debugf("Exiting backgroundRead: error reading readbuf: %s\n",
 				err)
 			return
 		}
@@ -144,63 +144,63 @@ func (s *Sequencer) backgroundRead() {
 		buf := bytes.NewReader(hdrBytes)
 		err = binary.Read(buf, binary.LittleEndian, &flags)
 		if err != nil {
-			fmt.Printf("Exiting backgroundRead: error reading flags: %s\n",
+			s.debugf("Exiting backgroundRead: error reading flags: %s\n",
 				err)
 			return
 		}
 		err = binary.Read(buf, binary.LittleEndian, &seq)
 		if err != nil {
-			fmt.Printf("Exiting backgroundRead: error reading seq: %s\n",
+			s.debugf("Exiting backgroundRead: error reading seq: %s\n",
 				err)
 			return
 		}
 		err = binary.Read(buf, binary.LittleEndian, &ack)
 		if err != nil {
-			fmt.Printf("Exiting backgroundRead: error reading ack: %s\n",
+			s.debugf("Exiting backgroundRead: error reading ack: %s\n",
 				err)
 			return
 		}
 		err = binary.Read(buf, binary.LittleEndian, &msgLen)
 		if err != nil {
-			fmt.Printf("Exiting backgroundRead: error reading msgLen: %s\n",
+			s.debugf("Exiting backgroundRead: error reading msgLen: %s\n",
 				err)
 			return
 		}
-		fmt.Printf("backgroundRead: seq %d ack %d len %d seqRxmt %d seqRcv %d\n",
+		s.debugf("backgroundRead: seq %d ack %d len %d seqRxmt %d seqRcv %d\n",
 			seq, ack, msgLen, s.seqRxmt, s.seqRcv)
 
 		s.xmitCond.L.Lock()
 		uDistanceRxmt := ack - s.seqRxmt
 		distanceRxmt := int16(uDistanceRxmt)
-		fmt.Printf("backgroundRead: ack is %d seq is %d uDistanceRxmt is %d distanceRxmt is %d s.seqRxmt %d s.seqXmt %d\n",
+		s.debugf("backgroundRead: ack is %d seq is %d uDistanceRxmt is %d distanceRxmt is %d s.seqRxmt %d s.seqXmt %d\n",
 			ack, seq, uDistanceRxmt, distanceRxmt, s.seqRxmt, s.seqXmt)
 		if distanceRxmt >= 0 {
 			if distanceRxmt > int16(len(s.rxmtBuf)) {
-				fmt.Printf("Exiting BackgroundRead: distanceRxmt (%d) > len(s.rxmtBuf) (%d)\n",
+				s.debugf("Exiting BackgroundRead: distanceRxmt (%d) > len(s.rxmtBuf) (%d)\n",
 					distanceRxmt,
 					int16(len(s.rxmtBuf)))
 				return
 			}
 			s.rxmtBuf = s.rxmtBuf[distanceRxmt:]
 			s.seqRxmt += uint16(distanceRxmt)
-			fmt.Printf("Updated received sequence by %d: len(s.rxmtBuf) = %d s.seqRxmt = %d\n",
+			s.debugf("Updated received sequence by %d: len(s.rxmtBuf) = %d s.seqRxmt = %d\n",
 				distanceRxmt, len(s.rxmtBuf), s.seqRxmt)
 		}
 		s.xmitCond.L.Unlock()
 
 		s.recvCond.L.Lock()
 		distanceSeq := int16(seq - s.seqRcv)
-		fmt.Printf("distanceSeq is %d seq is %d s.seqRcv is %d len(dataBuf) is %d\n",
+		s.debugf("distanceSeq is %d seq is %d s.seqRcv is %d len(dataBuf) is %d\n",
 			distanceSeq, seq, s.seqRcv, len(dataBuf))
 		if distanceSeq >= 0 {
 			if distanceSeq <= int16(len(dataBuf)) {
-				fmt.Printf("Databuf before shrink: %s\n",
+				s.debugf("Databuf before shrink: %s\n",
 					dataBuf)
 				dataBuf = dataBuf[distanceSeq:]
-				fmt.Printf("Databuf after shrink: %s\n",
+				s.debugf("Databuf after shrink: %s\n",
 					dataBuf)
 			}
-			fmt.Printf("backgroundRead: s.recvBuf %s databuf %s\n",
+			s.debugf("backgroundRead: s.recvBuf %s databuf %s\n",
 				s.recvBuf, dataBuf)
 			s.lastRcv = time.Now()
 			s.recvBuf = append(s.recvBuf, dataBuf...)
@@ -213,7 +213,7 @@ func (s *Sequencer) backgroundRead() {
 }
 
 func (s *Sequencer) Close() (err error) {
-	fmt.Printf("Write close() method\n")
+	s.debugf("Write close() method\n")
 	return nil
 }
 
@@ -222,7 +222,7 @@ func (s *Sequencer) Read(p []byte) (n int, err error) {
 		s.recvCond.L.Lock()
 		for {
 			n = len(s.recvBuf)
-			fmt.Printf("Sequencer.Read: len(p)=%d len(s.recvBuf) %d\n",
+			s.debugf("Sequencer.Read: len(p)=%d len(s.recvBuf) %d\n",
 				len(p), n)
 			if n != 0 {
 				break
@@ -236,12 +236,12 @@ func (s *Sequencer) Read(p []byte) (n int, err error) {
 		if n > len(p) {
 			n = len(p)
 		}
-		fmt.Printf("Sequencer.Read: len(s.recvBuf) %d s.recvBuf %s\n",
+		s.debugf("Sequencer.Read: len(s.recvBuf) %d s.recvBuf %s\n",
 			len(s.recvBuf), s.recvBuf)
 		copy(p, s.recvBuf)
 		s.recvBuf = s.recvBuf[n:]
 		s.recvCond.L.Unlock()
-		fmt.Printf("Returning Sequencer.Read: len(s.recvBuf) %d\n",
+		s.debugf("Returning Sequencer.Read: len(s.recvBuf) %d\n",
 			len(s.recvBuf))
 		return n, nil
 	}
@@ -253,7 +253,7 @@ func (s *Sequencer) Write(p []byte) (n int, err error) {
 	s.recvCond.L.Unlock()
 
 	s.xmitCond.L.Lock()
-	fmt.Printf("Sequencer.Write: len(s.rxmtBuf)=%d len(p)=%d seqXmt=%d seqRxmt=%d seqRcv=%d\n",
+	s.debugf("Sequencer.Write: len(s.rxmtBuf)=%d len(p)=%d seqXmt=%d seqRxmt=%d seqRcv=%d\n",
 		len(s.rxmtBuf), len(p), s.seqXmt, s.seqRxmt, ack)
 	seq := s.seqXmt
 	s.seqXmt += uint16(len(p))
@@ -297,4 +297,8 @@ func (s *Sequencer) Write(p []byte) (n int, err error) {
 	}
 
 	return len(p), nil
+}
+
+func (s *Sequencer) debugf(f string, args ...interface{}) {
+	//	fmt.Printf(f, args...)
 }
