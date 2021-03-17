5888d9b394303a6c17f7cc837c8ad78e3e0a8d17
Kevin Paul Herbert <kph@platinasystems.com>
2021-03-16 17:58:55 -0700

    squash me with -dnsaddr

--- cmd/install/debian.go
+++ cmd/install/debian.go
@@ -52,7 +52,7 @@ func (c *Command) debianInstall() (err error) {
 			"adduser {{ .AdminUser }} sudo",
 			"echo {{ .AdminUser }}:{{ .AdminPass }}|chpasswd",
 			"echo {{ .Hostname }}>/etc/hostname",
-			`{{if .DNSAddr }} sed -e s/^#DNS=$/DNS={{ .DNSAddr }{{end}}`,
+			`{{if .DNSAddr }} sed -i -e "s/^#DNS=$/DNS={{ .DNSAddr }}/" /etc/systemd/resolved.conf{{end}}`,
 			"systemctl enable systemd-resolved",
 		},
 		},
